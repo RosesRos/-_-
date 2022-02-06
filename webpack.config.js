a582a22c5cad576266feac4d234afb2b6b936582
FABIO214-BIT <rostranespinoza@gmail.com>
2022-02-06 13:55:55 +0300

    files_project

--- webpack.config.js
+++ webpack.config.js
@@ -30,9 +30,6 @@ const optimization = () => {
 
 const filenameJs = ext => isDev ? `js/[name].${ext}` : `js/[name].[contenthash].${ext}`;
 const filenameCss = ext => isDev ? `styles/[name].${ext}` : `styles/[name].[contenthash].${ext}`;
-//const filesImg = ext => isDev ? `images/[name].${ext}` : `images/[name].[contenthash].${ext}`;
-
-
 
 const cssLoaders = extra => {
   const loaders = [
@@ -114,7 +111,6 @@ module.exports = {
   optimization: optimization(),
   devServer: {
     historyApiFallback: true,
-    //contentBase: path.resolve(__dirname, './dist'),
     open: true,
     compress: true,
     hot: true,
@@ -135,25 +131,9 @@ module.exports = {
         test: /\.(s[ac]ss)$/,
         use: cssLoaders('sass-loader'),
       },
-      /*{
-        test: /\.svg$/,
-        use: [
-          {
-            loader: 'svg-inline-loader',
-          },
-        ],
-      },*/
       {
         test: /\.(?:jpg|jpeg|gif|png|ico|svg)$/i,
         type: 'asset/resource',
-        /*use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: filesImg('jpg'),
-            },
-          },
-        ],*/
       },
       {
         test: /\.(?:ttf|woff2|eot|otf)$/i,
